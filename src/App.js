c9efdf3685b3354b3435573699deed197cbecdaa
karthikeyan <karthikarthikeyan389@gmail.com>
2023-07-09 11:22:24 +0530

    add color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import New from './New';
+import './New.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <New/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/New.js
@@ -0,0 +1,6 @@
+import './New.css';
+
+function New() {
+    return (<h1 className="hello">hello</h1>)
+}
+export default New;
